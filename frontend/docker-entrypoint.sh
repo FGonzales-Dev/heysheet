01c1af7ec660c8e0d0e1615ca5a3a24c30408545
Francis Gonzales <francisgonzales.private@gmail.com>
2025-08-26 17:45:24 +0800

    test

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 set -e
-
-# Default for local runs
 : "${PORT:=8080}"
-
-# Render the Nginx config from the template using envsubst
-envsubst '${PORT}' \
-  < /etc/nginx/conf.d/default.conf.template \
-  > /etc/nginx/conf.d/default.conf
-
-# Start Nginx in the foreground
+envsubst '${PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 exec nginx -g 'daemon off;'
